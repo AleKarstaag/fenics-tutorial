0e2ce45248db2026d05c7acc589cd204190b5c74
Alessandro Beninati <alessandrobeninati98@gmail.com>
2021-07-20 10:27:34 +0200

    made ft06 compatible with updated fenics and simulated results in paraview

--- pub/python/vol1/ft06_elasticity.py
+++ pub/python/vol1/ft06_elasticity.py
@@ -8,21 +8,21 @@ its left end and deformed under its own weight.
 """
 
 from __future__ import print_function
-from fenics import *
+import fenics as FnX
 
 # Scaled variables
-L = 1; W = 0.2
-mu = 1
-rho = 1
+L = 2; W = 0.2
+mu = 2
+rho = 2
 delta = W/L
-gamma = 0.4*delta**2
-beta = 1.25
+gamma = 0.8*delta**2
+beta = 1.75
 lambda_ = beta
 g = gamma
 
 # Create mesh and define function space
-mesh = BoxMesh(Point(0, 0, 0), Point(L, W, W), 10, 3, 3)
-V = VectorFunctionSpace(mesh, 'P', 1)
+mesh = FnX.BoxMesh(FnX.Point(0, 0, 0), FnX.Point(L, W, W), 10, 3, 3)
+V = FnX.VectorFunctionSpace(mesh, 'P', 1)
 
 # Define boundary condition
 tol = 1E-14
@@ -30,52 +30,55 @@ tol = 1E-14
 def clamped_boundary(x, on_boundary):
     return on_boundary and x[0] < tol
 
-bc = DirichletBC(V, Constant((0, 0, 0)), clamped_boundary)
+bc = FnX.DirichletBC(V, FnX.Constant((0, 0, 0)), clamped_boundary)
 
 # Define strain and stress
 
 def epsilon(u):
-    return 0.5*(nabla_grad(u) + nabla_grad(u).T)
+    return 0.5*(FnX.nabla_grad(u) + FnX.nabla_grad(u).T)
     #return sym(nabla_grad(u))
 
 def sigma(u):
-    return lambda_*div(u)*Identity(d) + 2*mu*epsilon(u)
+    return lambda_*FnX.div(u)*FnX.Identity(d) + 2*mu*epsilon(u)
 
 # Define variational problem
-u = TrialFunction(V)
+u = FnX.TrialFunction(V)
 d = u.geometric_dimension()  # space dimension
-v = TestFunction(V)
-f = Constant((0, 0, -rho*g))
-T = Constant((0, 0, 0))
-a = inner(sigma(u), epsilon(v))*dx
-L = dot(f, v)*dx + dot(T, v)*ds
+v = FnX.TestFunction(V)
+f = FnX.Constant((0, 0, -rho*g))
+T = FnX.Constant((0, 0, 0))
+a = FnX.inner(sigma(u), epsilon(v))*FnX.dx
+L = FnX.dot(f, v)*FnX.dx + FnX.dot(T, v)*FnX.ds
 
 # Compute solution
-u = Function(V)
-solve(a == L, u, bc)
+u = FnX.Function(V)
+FnX.solve(a == L, u, bc)
 
 # Plot solution
-plot(u, title='Displacement', mode='displacement')
+# FnX.plot(u, title='Displacement', mode='displacement')
 
 # Plot stress
-s = sigma(u) - (1./3)*tr(sigma(u))*Identity(d)  # deviatoric stress
-von_Mises = sqrt(3./2*inner(s, s))
-V = FunctionSpace(mesh, 'P', 1)
-von_Mises = project(von_Mises, V)
-plot(von_Mises, title='Stress intensity')
+s = sigma(u) - (1./3)*FnX.tr(sigma(u))*FnX.Identity(d)  # deviatoric stress
+von_Mises = FnX.sqrt(3./2*FnX.inner(s, s))
+V = FnX.FunctionSpace(mesh, 'P', 1)
+von_Mises = FnX.project(von_Mises, V)
+# FnX.plot(von_Mises, title='Stress intensity')
 
 # Compute magnitude of displacement
-u_magnitude = sqrt(dot(u, u))
-u_magnitude = project(u_magnitude, V)
-plot(u_magnitude, 'Displacement magnitude')
+u_magnitude = FnX.sqrt(FnX.dot(u, u))
+u_magnitude = FnX.project(u_magnitude, V)
+FnX.plot(u_magnitude, 'Displacement magnitude')
+vertex_values_u_magnitude = u_magnitude.compute_vertex_values(mesh)
+import numpy as np
 print('min/max u:',
-      u_magnitude.vector().array().min(),
-      u_magnitude.vector().array().max())
+     np.min(np.abs(vertex_values_u_magnitude)),
+     np.max(np.abs(vertex_values_u_magnitude)))
 
 # Save solution to file in VTK format
-File('elasticity/displacement.pvd') << u
-File('elasticity/von_mises.pvd') << von_Mises
-File('elasticity/magnitude.pvd') << u_magnitude
+FnX.File('elasticity/displacement.pvd') << u
+FnX.File('elasticity/von_mises.pvd') << von_Mises
+FnX.File('elasticity/magnitude.pvd') << u_magnitude
 
 # Hold plot
-interactive()
+import matplotlib.pyplot as plt
+plt.show()
